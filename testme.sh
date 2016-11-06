5e76f991984457502844053076d0a5261716450c
Artur Skonecki <artur.skonecki@domo-safety.com>
2016-11-06 13:24:56 +0100

    next count

--- testme.sh
+++ testme.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 set -x
 cd /tmp
-wget $1/container_up
-cat container_up
+
+count=`hostname |cut -d '-' -f2`;
+
+if [ ! -z "${count##*[!0-9]*}" ]; then 
+ count=$(($count + 1))
+ echo "starting next container #$count"
+ wget $1/container_next?count=count&comment="next count is $count"
+ cat container_next
+else
+	echo "not starting next container"
+	wget $1/container_timestamp?comment="stop count $count"
+fi
+
 echo "sleeping..."
-sleep 1000
+sleep 9999999999;
